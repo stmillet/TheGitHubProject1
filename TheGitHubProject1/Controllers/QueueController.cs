c650b3d0dd52c8d3be35aef4af2a95f9a76a3775
Paige Harper <paige.e.harper@gmail.com>
2019-10-10 14:18:10 -0400

    new queue

--- TheGitHubProject1/Controllers/QueueController.cs
+++ TheGitHubProject1/Controllers/QueueController.cs
@@ -9,9 +9,71 @@ namespace TheGitHubProject1.Controllers
     public class QueueController : Controller
     {
         // GET: Queue
+        Queue<string> myQueue = new Queue<string>();
         public ActionResult Index()
         {
             return View();
+        }
+
+        //Function to add queue item
+        public ActionResult addItem()
+        {
+            myQueue.Enqueue("New Entry" + (myQueue.Count + 1));
+
+            return View("Index");
+        }
+
+        //Function to add queue items
+        public ActionResult addListItems()
+        {
+            myQueue.Clear();
+            for (int iCount = 0; iCount <= 2000; iCount++)
+            {
+                myQueue.Enqueue("New Entry" + (myQueue.Count + 1));
+            }
+
+            return View("Index");
+        }
+
+        //Function to display queue
+        public ActionResult displayQueue()
+        {
+            foreach (Object obj in myQueue)
+            {
+                Console.WriteLine(obj);
+            }
+
+            return View("Index");
+        }
+
+        //Function to delete item from queue
+        public ActionResult deleteFromQueue()
+        {
+
+            return View("Index");
+        }
+
+        //Function to clear queue
+        public ActionResult clearQueue()
+        {
+            myQueue.Clear();
+
+            return View("Index");
+        }
+
+        //Function to search queue
+        public ActionResult searchQueue()
+        {
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            //loop to do all the work
+
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
+            ViewBag.StopWatch = ts;
+
+            return View("Index");
         }
     }
 }
