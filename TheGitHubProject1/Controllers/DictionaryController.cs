d1c8bbeb5b2a286e2f47407c65b8ebb8af86a549
Spencer Millett <millett.spencer@gmail.com>
2019-10-10 12:07:29 -0600

    Added stuff to the dictionary View

--- TheGitHubProject1/Controllers/DictionaryController.cs
+++ TheGitHubProject1/Controllers/DictionaryController.cs
@@ -9,9 +9,21 @@ namespace TheGitHubProject1.Controllers
     public class DictionaryController : Controller
     {
         // GET: Dictionary
+        public Dictionary<int, int> theDictionary = new Dictionary<int, int>();
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult AddItem()
+        {
+            int numVariables = this.theDictionary.Count();
+            this.theDictionary.Add(numVariables, numVariables + 1);
+
+            ViewBag.Output = "<p>" + theDictionary[numVariables] + "</p>";
+
+            return View("Index");
         }
     }
 }
